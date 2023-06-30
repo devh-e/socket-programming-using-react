8f37cd65b2bd6c4ca2e0421840016374771971a3
HMG <jeongh@hyundai.com>
2023-06-30 12:52:41 +0900

    오타수정

--- part1/reactjs/react-ex/react-app/src/components/input/Input.js
+++ part1/reactjs/react-ex/react-app/src/components/input/Input.js
@@ -1,23 +1,23 @@
 // 1
 import React from "react";
-import styles from "./input.module.css";
+import styles from "./Input.module.css";
 
 // 2
 const Input = ({ onChange, onClick, value }) => {
-  return (
-    <form className={styles.inputBox} onSubmit={onClick}>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="What is your goal?"
-        onChange={onChange}
-        value={value}
-      />
-      <button type="submit" className={styles.button}>
-        Add
-      </button>
-    </form>
-  );
+    return (
+        <form className={styles.inputBox} onSubmit={onClick}>
+            <input
+                className={styles.input}
+                type="text"
+                placeholder="What is your goal?"
+                onChange={onChange}
+                value={value}
+            />
+            <button type="submit" className={styles.button}>
+                Send
+            </button>
+        </form>
+    );
 };
 
 export default Input;
